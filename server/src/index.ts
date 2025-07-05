528b610dbe7f031416ce86acdb5ffaffb687bb46
linganago <linganagoudamaligouda@gmail.com>
2025-07-05 19:20:04 +0530

    Lease and application controllers and routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,8 +10,8 @@ import { authMiddleware } from "./middleware/authMiddleware";
 import tenantRoutes from "./routes/tenantRoutes";
 import managerRoutes from "./routes/managerRoutes";
 import propertyRoutes from "./routes/propertyRoutes";
-// import leaseRoutes from "./routes/leaseRoutes";
-// import applicationRoutes from "./routes/applicationRoutes";
+import leaseRoutes from "./routes/leaseRoutes";
+import applicationRoutes from "./routes/applicationRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -29,9 +29,9 @@ app.get("/", (req, res) => {
   res.send("This is home route");
 });
 
-// app.use("/applications", applicationRoutes);
+app.use("/applications", applicationRoutes);
 app.use("/properties", propertyRoutes);
-// app.use("/leases", leaseRoutes);
+app.use("/leases", leaseRoutes);
 app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
 app.use("/managers", authMiddleware(["manager"]), managerRoutes);
 
